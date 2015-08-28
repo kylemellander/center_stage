4241d6d989d9e511506070108c535534e0fc7868
Kyle Mellander <kyle@koynas.com>
2015-08-27 21:33:29 -0700

    Add Initial files for Center Stage App

---
+++ lib/band.rb
@@ -0,0 +1,4 @@
+class Band < ActiveRecord::Base
+  has_many(:concerts)
+  has_many(:venues, through: :concerts)
+end
---
+++ spec/band_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe(Band) do
+  it{should have_many(:venues)}
+end
