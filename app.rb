878b57262b1eea8aa268c4fd0588fd5596b3acfb
Kyle Mellander <kyle@koynas.com>
2015-08-27 23:07:14 -0700

    Add page to display bands

--- app.rb
+++ app.rb
@@ -2,6 +2,11 @@ require("bundler/setup")
 Bundler.require(:default)
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 
-get ("/") do
+get("/") do
   erb(:index)
+end
+
+get("/bands") do
+  @bands = Band.all
+  erb(:bands)
 end
